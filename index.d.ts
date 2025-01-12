35fe43294e246af1a203d54470b307da8aebdd92
Daniel Arthur Gallagher <8526031+danieleloscozzese@users.noreply.github.com>
2025-01-12 11:38:29 +0100

    feat: align with upstream, fork name (#1)
    Integrates all the upstream changes, and forks the package with a new name and a dev version: notably drops the CJS version.

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2024 Adevinta
+// Copyright © 2025 Daniel Arthur Gallagher
 
-declare module "post-task" {
+declare module "@dagher/post-task" {
 	/**
-	 * Queues an arbitrary task to be executed in the browser, with the given priority.
-	 * Allows breaking up the work of potentially long-running tasks to avoid blocking the main thread.
+	 * Queues an arbitrary task to be scheduled for execution with the given
+	 * priority.
+	 *
+	 * Allows the discrete and prioritised queuing of tasks which if run serially
+	 * would block the main thread, but which do not have to be run immediately.
+	 *
+	 * @param task The callback to be executed.
+	 * @param priority The priority of the task, following the
+	 * Scheduler API.
+	 * @returns A promise that resolves when the task is executed,
+	 * in case it needs to be tracked.
 	 */
 	export default function postTask(
 		task: () => void,
